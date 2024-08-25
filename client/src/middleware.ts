b9886c05a0fe76615684f7d2d834817546d4cca7
sunil gautam <sunil904gautam@gmail.com>
2024-08-26 00:21:22 +0530

    feat: Added restricted route

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,2 +1,28 @@
-export { default } from "next-auth/middleware"
-export const config = { matcher: ["/"] }
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+
+  const isPublicPath = path === '/sign-in';
+
+  const token = request.cookies.get('next-auth.session-token')?.value || ''
+
+  if(isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/sign-in', request.nextUrl))
+  }
+    
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/sign-in',
+    '/profile',
+  ]
+}
